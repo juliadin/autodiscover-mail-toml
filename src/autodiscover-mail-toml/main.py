fc33a00aeecf03d7fe8526e4644166064e64f2b1
Julia Brunenberg <juli@monoro.se>
2024-02-21 22:55:03 +0100

    add comments

--- src/autodiscover-mail-toml/main.py
+++ src/autodiscover-mail-toml/main.py
@@ -1,3 +1,5 @@
+"""Small WSGI service to implement Mozilla autodiscovery for small mail providers"""
+
 #     Copyright (C) 2024  Julia Brunenberg
 #
 #     This program is free software: you can redistribute it and/or modify
@@ -15,6 +17,7 @@
 
 import pathlib
 import tomllib
+from typing import Any
 
 from fastapi import FastAPI, Response, HTTPException
 import jinja2
@@ -24,13 +27,39 @@ app = FastAPI()
 config_file = pathlib.Path("domains.toml")
 
 
-def get_config(domain: str = "", local: str = ""):
-    config = tomllib.loads(config_file.read_text())
+def get_config(domain: str = "", local: str = "") -> dict[str, str | list[str]]:
+    """Open, parse and layer the config for the current request
+    and return the context to be used in jinja
+
+    :param domain: The domain name of the email address requested
+    :type domain: str
+    :param local: The local part of the email address requested
+    :type local: str
+    :return: The jinja2.Context to be used in jinja
+    :rtype: dict[str, str | list[str]]
+    """
+    config = tomllib.loads(config_file.read_text(encoding="utf-8"))
     context = get_context(config, domain, local)
     return context
 
 
-def get_context(config, domain: str = "", local: str = ""):
+def get_context(
+    config: dict[Any, Any], domain: str = "", local: str = ""
+) -> dict[str, str | list[str]]:
+    """Layer the context to be used in jinja
+
+        user.<emailaddress> takes precedence over
+        domain.<domainname> takes precedence over
+        provider
+
+    :param config: The parsed toml as dict
+    :param domain: The domain name of the email address requested
+    :type domain: str
+    :param local: The local part of the email address requested
+    :type local: str
+    :return: The dict to be used in the jinja2 template
+    :rtype: dict[str, str | list[str]]
+    """
     if domain:
         if domain not in config.get("provider", {}).get("domains", []):
             if domain not in config.get("domain", {}).keys():
@@ -54,6 +83,13 @@ def get_context(config, domain: str = "", local: str = ""):
 
 
 def craft_xml(emailaddress: str = ""):
+    """Generate an XML string from the email address by tempating it with the resulting context
+
+    :param emailaddress: The email address of the request
+    :type emailaddress: str
+    :return: The XML string
+    :rtype: str
+    """
     try:
         local, domain = emailaddress.split("@")
     except ValueError:
@@ -91,11 +127,7 @@ def craft_xml(emailaddress: str = ""):
     ).render(config)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
 @app.get("/mail/config-v1.1.xml")
 async def get_xml(emailaddress: str = ""):
+    """the xml document for auto-configuration"""
     return Response(craft_xml(emailaddress=emailaddress), media_type="application/xml")
